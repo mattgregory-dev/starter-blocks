3927fd8289a3ec3d9f31df27d622efacc9c0615a
mattgregory-dev <3980526+mattgregory-dev@users.noreply.github.com>
2026-08-02 21:02:49 -0700

    feat: add spotlight block and starter

--- inc/blocks.php
+++ inc/blocks.php
@@ -18,5 +18,6 @@ function sb_register_blocks() {
 	register_block_type( get_template_directory() . '/build/intro-section' );
 	register_block_type( get_template_directory() . '/build/cta-band' );
 	register_block_type( get_template_directory() . '/build/checklist-section' );
+	register_block_type( get_template_directory() . '/build/spotlight' );
 }
 add_action( 'init', 'sb_register_blocks' );
